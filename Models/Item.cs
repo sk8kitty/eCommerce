6bb9e727ace7f4c9770b6f7d3e14f749a0b4599f
Teddy <dudetotheleft@gmail.com>
2023-08-02 14:03:46 -0700

    Created model class for Items

---
+++ Models/Item.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
+{
+    /// <summary>
+    /// Represents an individual bakery item
+    /// </summary>
+    public class Item
+    {
+        /// <summary>
+        /// Unique identifier for each item
+        /// </summary>
+        [Key]
+        public int ItemId { get; set; }
+
+        /// <summary>
+        /// Name of the bakery item
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Explanation of what the bakery item is
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Sales price of the bakery item
+        /// </summary>
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+    }
+}
