496e3720e99b3ba654e6021a526463990bb76be8
Teddy <dudetotheleft@gmail.com>
2023-08-02 15:05:16 -0700

    Configured database context

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
+using eCommerce.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<BakeryItemContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
