251cbe7863099d19bdff4cdf9b496115c537d9d3
Teddy <dudetotheleft@gmail.com>
2023-08-24 11:01:28 -0700

    Set up ItemController and functional Create methods

---
+++ Controllers/ItemController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using eCommerce.Models;
+using eCommerce.Data;
+
+namespace eCommerce.Controllers
+{
+    public class ItemController : Controller
+    {
+        private BakeryItemContext _context;
+
+        public ItemController(BakeryItemContext context)
+        {
+           _context = context;
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Item i) 
+        {
+            if (ModelState.IsValid) 
+            {
+                _context.Items.Add(i);
+                _context.SaveChanges();
+
+                ViewData["Message"] = $"{i.Title} was added successfully!";
+
+                return View();
+            }
+
+            return View();
+        }
+    }
+}
