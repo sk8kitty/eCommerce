1d36ada7d1bff08e94e4e1fd3e8dcf15643ece54
Teddy <dudetotheleft@gmail.com>
2023-08-30 02:27:21 -0700

    Refactored item index to use a ViewModel

---
+++ Models/ItemCatalogViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace eCommerce.Models
+{
+    public class ItemCatalogViewModel
+    {
+        public ItemCatalogViewModel(List<Item> items, int lastPage, int currPage)
+        {
+            Items = items;
+            LastPage = lastPage;
+            CurrentPage = currPage;
+        }
+
+
+        public List<Item> Items { get; set; }
+
+        /// <summary>
+        /// Last page of the catalog. Quotient of total num of items / num items displayed per page.
+        /// </summary>
+        public int LastPage { get; set; }
+
+        /// <summary>
+        /// Page the user is currently viewing.
+        /// </summary>
+        public int CurrentPage { get; set; }
+    }
+}
